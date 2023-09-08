c022da57d12984cde27deca770eda20a34877635
learnttuce0321 <lettucegood2@gmail.com>
2023-09-08 13:10:15 +0900

    fix: 좋아요 버그 수정

--- controll/alcoholListDetail/CalcoholListDetail.js
+++ controll/alcoholListDetail/CalcoholListDetail.js
@@ -6,82 +6,65 @@ import path from 'path';
 const __dirname = path.join(path.resolve(), 'public');
 
 const CAlcoholListDetail = (req, res) => {
-	console.log('dfs', req.params);
-	models.AlcoholList.findOne({
-		where: {
-			id: req.params.id,
-		},
-	}).then((result) => {
-		console.log('asdf', result);
-		res.render('alcoholListDetail/alcoholListDetail', {data: result});
-	});
+    // console.log('dfs', req.params);
+    models.AlcoholList.findOne({
+        where: {
+            id: req.params.id,
+        },
+    }).then((result) => {
+        // console.log(result);
+        res.render('alcoholListDetail/alcoholListDetail', { data: result });
+    });
 };
 
 const CalcoholListLikePost = async (req, res) => {
-	console.log(req.cookies.userIdCookie);
-	let userId = req.cookies.userIdCookie;
-	if (userId) {
-		const result = await models.LikeAlcohol.create({
-			userId: userId,
-			alcoholId: req.params.id,
-		});
+    const result = await models.LikeAlcohol.create({
+        userId: req.cookies.userIdCookie,
+        alcoholId: req.params.id,
+    });
 
-		if (result) {
-			res.json({result: true});
-		} else {
-			res.json({result: false});
-		}
-	} else {
-		res.json({result: false});
-	}
+    if (result) {
+        res.json({ result: true });
+    } else {
+        res.json({ result: false });
+    }
 };
 
 const CfindAlcoholListLikePost = async (req, res) => {
-	console.log(req.cookies.userIdCookie);
-	if (req.cookies.userIdCookie) {
-		let userId = req.cookies.userIdCookie;
-		const result = await models.LikeAlcohol.findOne({
-			where: {
-				userId: userId,
-				alcoholId: req.params.id,
-			},
-		});
+    const result = await models.LikeAlcohol.findOne({
+        where: {
+            userId: req.cookies.userIdCookie,
+            alcoholId: req.params.id,
+        },
+    });
 
-		if (result) {
-			res.json({result: true});
-		} else {
-			res.json({result: false});
-		}
-	} else {
-		res.json({result: false});
-	}
+    if (result) {
+        res.json({ result: true });
+    } else {
+        res.json({ result: false });
+    }
 };
 
 const CdeleteAlcoholListLikePost = async (req, res) => {
-	if (req.cookies.userIdCookie) {
-		let userId = req.cookies.userIdCookie;
-		const destroyTarget = await models.LikeAlcohol.findOne({
-			where: {
-				userId: userId,
-				alcoholId: req.params.id,
-			},
-		});
+    const destroyTarget = await models.LikeAlcohol.findOne({
+        where: {
+            userId: req.cookies.userIdCookie,
+            alcoholId: req.params.id,
+        },
+    });
 
-		const result = await destroyTarget.destroy({
-			where: {
-				userId: userId,
-				alcoholId: req.params.id,
-			},
-		});
-		res.json({result: true});
-	} else {
-		res.json({result: false});
-	}
+    const result = await destroyTarget.destroy({
+        where: {
+            userId: req.cookies.userIdCookie,
+            alcoholId: req.params.id,
+        },
+    });
+    res.json({ result: true });
 };
 
 export {
-	CAlcoholListDetail,
-	CalcoholListLikePost,
-	CfindAlcoholListLikePost,
-	CdeleteAlcoholListLikePost,
+    CAlcoholListDetail,
+    CalcoholListLikePost,
+    CfindAlcoholListLikePost,
+    CdeleteAlcoholListLikePost,
 };
