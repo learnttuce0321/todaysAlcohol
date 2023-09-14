75ef406737ee08ad437bbf3e5b12fc4e8fd857f9
learnttuce0321 <lettucegood2@gmail.com>
2023-09-14 13:47:58 +0900

    refactor: 함수 수정

--- public/findFriendPage/findFriendPage.js
+++ public/findFriendPage/findFriendPage.js
@@ -23,7 +23,8 @@ const participate = async (roomId, e) => {
         alert('로그인 후 사용 가능합니다.');
         window.location.href = '/login';
     }
-})(async () => {
+})();
+(async () => {
     const result = await axios({
         method: 'POST',
         url: '/find-friends/participation',
